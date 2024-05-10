9ba488b33f3b26fcd0265e8cb3b539b67a072b7a
Milluuu <shma.leshh@gmail.com>
2024-05-10 17:12:32 +1200

    Initialized repo

---
+++ src/main/java/MathQuizV5/MathQuizProject.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package MathQuizV5;
+
+/**
+ *
+ * @author Milu
+ */
+public class MathQuizProject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
