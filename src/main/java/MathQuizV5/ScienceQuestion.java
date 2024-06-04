f07f640234cdd03f8ac4ae72d4e65cf4fd42d0d6
Adrian Chand <adidasiscool12345@gmail.com>
2024-06-04 19:15:45 +1200

    Added timer and end of game panel

--- src/main/java/MathQuizV5/ScienceQuestion.java
+++ src/main/java/MathQuizV5/ScienceQuestion.java
@@ -11,20 +11,10 @@ package MathQuizV5;
 public class ScienceQuestion extends Question{ 
     public ScienceQuestion() { 
         super("", 0); 
-        switch(generateNumber(2)){ 
-            case 0: 
-                generateSpeedQuestion(); 
-                break; 
-            case 1: 
-                generateMolarMassQuestion(); 
-                break; 
-            default: 
-                System.out.println("Invalid input"); 
-                break; 
-        } 
+        generateSpeedQuestion();
     } 
 
-    private void generateSpeedQuestion(){ 
+    private synchronized void generateSpeedQuestion(){ //Remove synchronized if needed
         int distance = generateNumber(60); 
         int time = generateNumber(5); 
         double speed = distance / time;  
@@ -34,18 +24,6 @@ public class ScienceQuestion extends Question{
                 + "on a given training day. In " + time + ", you covered a distance of " + distance + " kilom" 
                 + "eters on foot.</html>"; 
         this.correctAnswer = speed; 
-    } 
-
-    private void generateMolarMassQuestion(){ 
-        int volume = generateNumber(10); 
-        int concentration = generateNumber(10); 
-        double molarMass = concentration / volume; 
-
-        this.questionText = "<html>You are working on an experiment in a chemical lab to find the mass of a solute <br>" 
-                + "required to produce a particular concentration of a solution. You are informed that the needed <br>" 
-                + "volume (v) of the solution is " + volume + "liters (L), and that the concentration (c) of the solution should \n" 
-                + "be "+ concentration + " grams per liter (g/L).<html>"; 
-        this.correctAnswer = molarMass; 
     } 
     
     private int generateNumber(int range) { 
